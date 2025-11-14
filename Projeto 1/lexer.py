594b273ec2dc1bbf5404c51d4421b9d2038fdf88
Giovana Piazza <135471104+giovanapiazza@users.noreply.github.com>
2025-11-14 10:37:30 -0300

    Update lexer.py

--- Projeto 1/lexer.py
+++ Projeto 1/lexer.py
@@ -1,5 +1,6 @@
 import ply.lex as lex
 
+# Palavras reservadas da linguagem
 reserved = {
     'defun' : 'DEFUN',
     'if'    : 'IF',
@@ -14,15 +15,29 @@ reserved = {
 
 # Lista de tokens
 tokens = [
-    'LPAREN', 'RPAREN', 'QUOTE', 'NUMBER', 'ID', 'DEFUN',
-    'PLUS', 'MINUS', 'TIMES', 'DIV', 'DIVINT', 'MOD', 'EXP',
-    'LT', 'GT', 'LE', 'GE', 'EQ', 'NE', 'LIST'
-    ] + list(reserved.values())
+    'LPAREN', 'RPAREN', 'QUOTE',
+    'NUMBER', 'ID',
 
-# Regulares simples
+    # Operadores aritméticos
+    'PLUS', 'MINUS', 'TIMES', 'DIV', 'DIVINT', 'MOD', 'EXP',
+
+    # Operadores relacionais
+    'LT', 'GT', 'LE', 'GE', 'EQUALS', 'NE',
+] + list(reserved.values())
+
+# Tokens MULTICARACTERE primeiro
+t_LE     = r'<='
+t_GE     = r'>='
+t_NE     = r'!='
+
+# Tokens simples
+t_LT     = r'<'
+t_GT     = r'>'
+t_EQUALS = r'='
+
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
-t_QUOTE = r"\'"
+t_QUOTE  = r"\'"
 
 t_PLUS   = r'\+'
 t_MINUS  = r'-'
@@ -32,16 +47,10 @@ t_DIVINT = r'div'
 t_MOD    = r'mod'
 t_EXP    = r'exp'
 
-t_LT     = r'<'
-t_GT     = r'>'
-t_LE     = r'<='
-t_GE     = r'>='
-t_EQ     = r'='
-t_NE     = r'!='
+# Ignorar espaços e quebras de linha
+t_ignore = " \t\n\r"
 
-t_ignore = "\t\n\r\ "
 
-# Tokens mais complexos
 def t_NUMBER(t):
     r'0|([1-9][0-9]*)'
     t.value = int(t.value)
@@ -49,16 +58,17 @@ def t_NUMBER(t):
 
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t.type = reserved.get(t.value, 'ID')  # Verifica se é palavra reservada
     return t
-
-def t_error(t):
-    print(f"Caractere inválido: {t.value[0]}")
-    t.lexer.skip(1)
 
 def t_COMMENT(t):
     r'\;.*'
-    pass
+    pass  # Apenas ignora comentários iniciados com ;
 
+# Erro lexical
+def t_error(t):
+    print(f"Caractere inválido: {t.value[0]}")
+    t.lexer.skip(1)
 
 if __name__ == "__main__":
     lexer = lex.lex()
@@ -66,5 +76,6 @@ if __name__ == "__main__":
     data = "( defun sqr (div 12 0 2 300))"
 
     lexer.input(data)
+
     for tok in lexer:
         print(tok)
